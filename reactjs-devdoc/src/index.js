a73922c4e898d9d061208a5c7a34c5606fb5f705
Pritesh_PC <priteshrane01@gmail.com>
2020-07-03 15:47:31 +0530

    Introduction from documentation

--- reactjs-devdoc/src/index.js
+++ reactjs-devdoc/src/index.js
@@ -1,14 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import logo from './logo.svg';
+
+
+
+// Embedding expression in JSX
+// example 1
+const element1 = <h1>Hello World!</h1>;
+
+// example 2
+const name ='Pritesh Rane'
+const element2 = <h1>Hello, {name}</h1>;
+
+// example 3
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName
+}
+
+const user = {
+  firstName: 'Pritesh',
+  lastName: 'Rane',
+  image: logo
+}
+
+const element3 = (
+  <h1>
+    Hello, {formatName(user)}
+  </h1>
+)
+
+
+
+// JSX is an Expression Too
+function getGreeting(user) {
+  if (user) {
+  return <h1>Hello, {formatName(user)}!</h1>
+  }
+  return <h1>Hello, Stranger.</h1>
+}
+
+const element4 = getGreeting(user)
+
+// Specifiying attributes with JSX
+const element5 = <img src={user.image}></img>
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // element3,
+  // element4,
+  element5,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
